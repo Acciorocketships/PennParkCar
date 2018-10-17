98473efe77ed32be1ad0c313402fda07cda1e7e1
SphericalCow <acciorocketships@gmail.com>
2018-10-17 02:52:05 -0400

    updated 3d transform

--- FindRoadEdge.py
+++ FindRoadEdge.py
@@ -18,27 +18,31 @@ class RoadFinder:
 
 
 	def setRT(self,heightoffset=0.1,sideoffset=0,forwardoffset=0,yawoffset=0,pitchoffset=0,rolloffset=0):
-		self.camT = np.array([forwardoffset, heightoffset, sideoffset]) # x (forward), y (up), z (side)
-		invyaw = np.array([[cos(-yawoffset), -sin(-yawoffset), 0],[sin(-yawoffset), cos(-yawoffset), 0],[0, 0, 1]])
-		invpitch = np.array([[cos(-pitchoffset), 0, sin(-pitchoffset)],[0, 1, 0],[-sin(-pitchoffset), 0, cos(-pitchoffset)]])
-		invroll = np.array([[1, 0, 0],[0, cos(-rolloffset), -sin(-rolloffset)],[0, sin(-rolloffset), cos(-rolloffset)]])
-		self.camR = invroll * invpitch * invyaw
+		yawoffset = pi/180 * yawoffset
+		pitchoffset = pi/180 * pitchoffset
+		rolloffset = pi/180 * rolloffset
+		self.camT = np.array([forwardoffset, sideoffset, -heightoffset]) # x (forward), y (right), z (down)
+		yaw = np.array([[cos(yawoffset), -sin(yawoffset), 0], [sin(yawoffset), cos(yawoffset), 0], [0, 0, 1]])
+		pitch = np.array([[cos(pitchoffset), 0, sin(pitchoffset)], [0, 1, 0], [-sin(pitchoffset), 0, cos(pitchoffset)]])
+		roll = np.array([[1, 0, 0], [0, cos(rolloffset), -sin(rolloffset)], [0, sin(rolloffset), cos(rolloffset)]])
+		self.camR = yaw @ pitch @ roll
 
 
 	def filterColor(self, img, ranges=(255,25,255)):
 		frontpoint = (0.5, 0.1) # fractions of total resolution in (x,y)
 		avgsize = 5 # number of pixels to average
 		blursize = 25
-		# Calculations
+		# Convert to HSV
 		imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 		imghsv = cv2.GaussianBlur(imghsv,(blursize,blursize),0)
+		# Get color of road
 		frontpoint = (int(imghsv.shape[0]*(1-frontpoint[1])), int(imghsv.shape[1]*frontpoint[0]))
 		frontregion = imghsv[frontpoint[0]-avgsize//2:frontpoint[0]+avgsize//2, frontpoint[1]-avgsize//2:frontpoint[1]+avgsize//2, 0:3]
 		avgcolor = np.average(np.average(frontregion,axis=1),axis=0).flatten()
+		# Color Threshold
 		lowerthres = np.array([max(avgcolor[0]-ranges[0], 0), max(avgcolor[1]-ranges[1], 0), max(avgcolor[2]-ranges[2], 0)], dtype=np.uint8)
 		upperthres = np.array([min(avgcolor[1]+ranges[0], 255), min(avgcolor[1]+ranges[1], 255), min(avgcolor[2]+ranges[2], 255)], dtype=np.uint8)
 		mask = cv2.inRange(imghsv, lowerthres, upperthres)
-		# mask = cv2.GaussianBlur(mask,(blursize,blursize),0)
 		return mask
 
 
@@ -62,7 +66,6 @@ class RoadFinder:
 		# Get contours
 		mask.dtype = np.uint8
 		im2, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 		# Get largest contour
 		maxarea = 0
 		maxcontour = contours[0]
@@ -71,16 +74,13 @@ class RoadFinder:
 			if area > maxarea:
 				maxarea = area
 				maxcontour = contour
-
 		# Simplify contour
 		epsilon = 0.06*self.res[0]
 		simplecontour = cv2.approxPolyDP(maxcontour,epsilon,True)
-
 		# Visualization
 		if draw is not None:
 			cv2.drawContours(draw, [maxcontour], 0, (255,0,0), 8)
 			cv2.drawContours(draw, [simplecontour], 0, (0,0,255), 8)
-
 		return simplecontour
 
 
@@ -113,22 +113,22 @@ class RoadFinder:
 
 
 	def img2xy(self,points):
-		# input point has origin in the top left, (x,y) order, increasing down and to the right
-		# output point has origin in the center, (x,y) order, increasing up and to the right
+		# input point has origin in the top left, (y,z) order, increasing down and to the right
+		# output point has origin in the center, (y,z) order, increasing up and to the right
 		if points is None:
 			return None
 		output = []
 		if type(points) != list and type(points) != tuple:
 			points = [points]
 		for point in points:
-			point = np.array([point[0] - self.res[1]/2, self.res[0]*3/2 - point[1]])
+			point = np.array([point[0] - self.res[1]/2, point[1] - self.res[0]/2])
 			output.append(point)
 		if type(points) == tuple:
 			output = tuple(output)
 		return output
 
 
-	def transform(self,points,planeN=[0,1,0],planeT=[0,0,0]):
+	def transform(self,points,planeN=[0,0,1],planeT=[0,0,0]):
 		if points is None:
 			return None
 		planeN = np.reshape(planeN,(3,))
@@ -139,7 +139,7 @@ class RoadFinder:
 			points = [points]
 		for point in points:
 			point = np.reshape(point,(2,))
-			point = np.concatenate((point * self.pix2dist, [self.camFocal]), axis=0)
+			point = np.concatenate(([self.camFocal], point * self.pix2dist), axis=0)
 			point = self.camR @ point # rotate to the world frame
 			c = (-np.dot(self.camT,planeN) + np.dot(planeT,planeN)) / np.dot(point,planeN)
 			transformed = c * point + self.camT
@@ -154,6 +154,7 @@ if __name__ == '__main__':
 	stream = Stream(mode='img',src='Files/Pictures')
 	resolution = (720,1280)
 	roadfinder.setCam(res=resolution)
+	roadfinder.setRT(heightoffset=0.1,pitchoffset=-15)
 	for img in stream:
 		img = Stream.resize(img,resolution)
 		imgshow = np.copy(img)
@@ -162,9 +163,10 @@ if __name__ == '__main__':
 		hull = roadfinder.hull(mask, draw=imgshow)
 		edge = roadfinder.segment(hull)
 		worldedge = roadfinder.transform(roadfinder.img2xy(edge))
-		print(worldedge)
+		print("worldedge", worldedge)
 		dtime = time.time() - starttime
-		# print(dtime)
+		print("time", dtime)
+		# import pdb; pdb.set_trace()
 		maskedimg = Stream.mask(mask,imgshow,alpha=0.3)
 		if worldedge is not None:
 			maskedimg = Stream.mark(maskedimg, (tuple(edge[0]),tuple(edge[1])), color=(0,255,0))
