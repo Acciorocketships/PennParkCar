d9f92fdb8543d33dc9092649a1504bd16cfed97c
Ryan Kortvelesy <acciorocketships@gmail.com>
2018-10-30 08:48:12 -1000

    added to MainLoop

--- Filter.py
+++ Filter.py
@@ -2,14 +2,18 @@ from time import time
 
 class Filter:
 
-	def __init__(self):
+	def __init__(self,wc=1):
 		self.lowmeas = 0
 		self.highmeas = 0
 		self.filterval = 0
 		self.val = 0
-		self.wc = 1
+		self.wc = wc
+		self.time = time()
 
-	def predict(self,update=True):
+	def predict(self,dt=None,update=True):
+		if dt is None:
+			dt = time() - self.time
+		self.time = time()
 		alpha = 1 / (1 + self.wc * dt)
 		filterval = (alpha) * (self.filterval) + (1-alpha) * (self.lowmeas - self.highmeas)
 		if update:
