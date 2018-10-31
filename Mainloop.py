e0011524e95314ae65a85910ebff57d9df7e46d6
SphericalCow <acciorocketships@gmail.com>
2018-10-31 04:12:58 -0400

    added to Mainloop

--- Mainloop.py
+++ Mainloop.py
@@ -11,15 +11,22 @@ class MainLoop:
 	def __init__(self):
 		self.map = Map()
 		self.roadfinder = RoadFinder()
-		self.showimg = False
 		self.imgstream = Stream(mode='img',src='Files/Pictures')
 		self.threads = {}
 		# Variables
-		self.vars = {'psiCAM': 0, 'psiIMU': 0, 'psiRoad': 0, 'posCAM': 0, 'posGPS': 0, 'vel': 0}
-		self.localpos = 0
-		self.localpsi = 0
+		self.localvars = {'psiCAM': 0, 'psiIMU': 0, 'posCAM': 0, 'vel': 0,
+						  'psi': 0, 'pos': 0, 'side': None}
+		self.globalvars = {'posGPS': 0, 'psiGPS': 0, 'psiGPSconfidence': 0, 
+						   'psiRoad': 0, 'progress': 0, 'intersection': False,
+						   'roadStart': np.array([0,0]), 'roadEnd': np.array([0,0])}
+		# Outputs
+		self.psid = 0
+		self.veld = 0
 		# Options
 		self.desiredEdgeDist = 1
+		self.intersectionRadius = 5
+		self.showimg = False
+		self.print = False
 
 
 	def run(self):
@@ -33,15 +40,22 @@ class MainLoop:
 			# Process Image
 			edge = self.roadfinder.find(img,show=self.showimg) # edge = ((x1,y1),(x2,y2))
 			# Get Distance from edge and Psi relative to edge
-			dist = (edge[1][0]*edge[0][1] + edge[1][1]*edge[0][0]) / ( (edge[1][1]-edge[0][1])**2 + (edge[1][0]-edge[0][0])**2 )
-			psi = -atan2(edge[1][0]-edge[0][0], edge[1][1]-edge[0][1])
-			psi = psi + pi if (psi < -pi/2) else psi - pi if (psi > pi/2) else psi # normalize
-			line = lambda point: tan(psi)*(point[1]-edge[0][1]) + edge[0][0] # x = 1/m * (y-y1) + x1
-			if x > line((0,0)):
-				self.vars['posCAM'] = self.desiredEdgeDist - dist
+			if edge is not None:
+				dist = (edge[1][0]*edge[0][1] + edge[1][1]*edge[0][0]) / ( (edge[1][1]-edge[0][1])**2 + (edge[1][0]-edge[0][0])**2 )
+				psi = -atan2(edge[1][1]-edge[0][1], edge[1][0]-edge[0][0])
+				psi = psi + pi if (psi < -pi/2) else psi - pi if (psi > pi/2) else psi # normalize
+				line = lambda point: tan(psi)*(point[1]-edge[0][1]) + edge[0][0] # x = 1/m * (y-y1) + x1
+				if line((0,0)) > 0:
+					self.localvars['posCAM'] = self.desiredEdgeDist - dist
+					self.localvars['side'] = 'right'
+				else:
+					self.localvars['posCAM'] = dist - self.desiredEdgeDist
+					self.localvars['side'] = 'right'
+				self.localvars['psiCAM'] = psi
 			else:
-				self.vars['posCAM'] = dist - self.desiredEdgeDist
-			self.vars['psiCAM'] = psi
+				self.localvars['posCAM'] = 0
+				self.localvars['psiCAM'] = 0
+				self.localvars['side'] = None
 
 
 	def filter(self):
@@ -55,7 +69,24 @@ class MainLoop:
 			psi.lowmeas = self.vars['psiCAM']
 			psi.highmeas = self.vars['psiIMU']
 			psi.update(dt=dt)
-			self.localpsi = psi.val
+			self.localvars['psi'] = psi.val
 			pos.lowmeas = self.vars['posCAM']
-			pos.highmeas = (self.vars['vel'] * dt) * np.array([cos(self.localpsi), sin(self.localpsi)])
+			pos.highmeas = self.vars['vel'] * dt * sin(self.localvars['psi'])
+			pos.update(dt=dt)
+			self.localvars['pos'] = pos.val
+
+
+	def gps(self):
+		# NearestRoad to rectify gps pos and calculate other globalvars
+		# calculate long and short EMA of gps. subtract to get vector. direction is psi, and magnitude is confidence.
+		# perhaps abs value of dot product of vector with road direction for confidence?
+		pass
+
+
+	def receive(self):
+		pass
+
+
+	def send(self):
+		pass
 
