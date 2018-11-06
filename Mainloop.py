16441e1e0424736391b25c3e6048fac66c2ac0d4
SphericalCow <acciorocketships@gmail.com>
2018-11-06 13:39:49 -0500

    updated mainloop

--- Mainloop.py
+++ Mainloop.py
@@ -2,9 +2,10 @@ from threading import Thread
 from Filter import *
 from FindRoadEdge import *
 from ImgStream import *
-from NearestRoad import *
+from Map import *
 from math import *
 from time import time, sleep
+import numpy as np
 
 class MainLoop:
 
@@ -14,9 +15,11 @@ class MainLoop:
 		self.imgstream = Stream(mode='img',src='Files/Pictures')
 		self.threads = {}
 		# Variables
+		self.inputs = {'psiIMUdot': 0, 'posGPS': (0,0)}
 		self.localvars = {'psiCAM': 0, 'psiIMU': 0, 'posCAM': 0, 'vel': 0,
 						  'psi': 0, 'pos': 0, 'side': None}
-		self.globalvars = {'posGPS': 0, 'psiGPS': 0, 'psiGPSconfidence': 0, 
+		self.globalvars = {'posGPS': 0, 'psiGPS': 0, 
+						   'psiGPSconfidence': 0, 'posGPSconfidence': 0,
 						   'psiRoad': 0, 'progress': 0, 'intersection': False,
 						   'roadStart': np.array([0,0]), 'roadEnd': np.array([0,0])}
 		# Outputs
@@ -80,7 +83,32 @@ class MainLoop:
 		# NearestRoad to rectify gps pos and calculate other globalvars
 		# calculate long and short EMA of gps. subtract to get vector. direction is psi, and magnitude is confidence.
 		# perhaps abs value of dot product of vector with road direction for confidence?
-		pass
+		# if high confidence and going in the opposite direction, then turn around
+		looptime = 0.2
+		roadMap = Map()
+		gpsEMAlong = np.array([0,0])
+		gpsEMAshort = np.array([0,0])
+		longEMAalpha = 0.05 * looptime
+		shortEMAalpha = 0.5 * looptime
+
+		while True:
+			mapData = roadMap.predictPos(roadMap.deg2meters(self.inputs['posGPS']))
+			self.globalvars['roadStart'] = mapData['Road Start']
+			self.globalvars['roadEnd'] = mapData['Road End']
+			self.globalvars['posGPS'] = mapData['Prediction']
+			gpsEMAlong = longEMAalpha * self.globalvars['posGPS'] + (1-longEMAalpha) * gpsEMAlong
+			gpsEMAshort = shortEMAalpha * self.globalvars['posGPS'] + (1-shortEMAalpha) * gpsEMAshort
+			vecGPS = gpsEMAshort - gpsEMAlong
+			self.globalvars['psiGPS'] = atan2(vecGPS[1],vecGPS[0])
+			vecRoad = mapData['Road Start'] - mapData['Road End']
+			roadLength = np.linalg.norm(vecRoad)
+			vecRoad = vecroad / roadLength
+			self.globalvars['psiGPSconfidence'] = tanh(0.1*np.dot(vecGPS,vecRoad))
+			self.globalvars['posGPSconfidence'] = exp(-0.1*mapData['Error'])
+			self.globalvars['progress'] = mapData["Fraction Along Road"]
+			self.globalvars['intersection'] = ((self.globalvars['progress'] * roadLength < intersectionRadius) or
+			 								  ((1-self.globalvars['progress']) * roadLength < intersectionRadius))
+
 
 
 	def receive(self):
